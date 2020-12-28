58d6ecb532912ada00f29f3bea24e182802c4ad6
moxy <moxy7@qq.com>
2020-12-28 14:31:04 +0800

    week 17 | done

---
+++ Week_17/test-demo/test/test.js
@@ -0,0 +1,20 @@
+// var assert = require('assert');
+
+// import {add, mul} from "../add.js";
+
+// // var add = require('../add.js').add;
+// // var mul = require('../add.js').mul;
+
+// describe("add function testing", function(){
+//     it('1 + 2 shoule be 3', function() {
+//         assert.equal(add(1,2), 3);
+//     }); 
+
+//     it('-1 + -6 shoule be -7', function() {
+//         assert.equal(add(-1,-6), -7);
+//     }); 
+
+//     it('-2 * -6 shoule be 12', function() {
+//         assert.equal(mul(-2,-6), 12);
+//     }); 
+// })
